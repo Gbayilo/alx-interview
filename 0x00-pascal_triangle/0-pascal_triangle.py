46fad7d7c9a217672d2fbcc9e33a7cb14f3ae09f
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-04 12:50:57 +0100

    Add new solution to task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""
-Returns a list of lists of integers representing the Pascal’s triangle of n.
-"""
+'''A module for working with Pascal's triangle.
+'''
 
 
 def pascal_triangle(n):
-    """Implements the pascal triangle."""
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
     triangle = []
     if type(n) is not int or n <= 0:
         return triangle
-    for index in range(n):
-        layer = []
-        for jndex in range(index + 1):
-            if jndex == 0 or jndex == index:
-                layer.append(1)
-            elif index > 0 and jndex > 0:
-                layer.append(triangle[index - 1][jndex - 1] +
-                            triangle[index - 1][jndex])
-        triangle.append(layer)
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
     return triangle
