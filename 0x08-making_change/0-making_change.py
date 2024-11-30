eeed391e87063461eab96bd0aba19da15db0300f
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-11-30 14:45:13 +0100

    Add solution to task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,19 +4,19 @@
 
 def makeChange(coins, total):
     """ Implementation of the make change."""
-    coins.sort(reverse=True)
-    count = 0
-    for coin in coins:
-        while total >= coin:
-            total -= coin
-            count += 1
-
-    if total == 0:
-        return count
-    else:
-        return -1
-
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
